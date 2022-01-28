8fb851ff16bd37011ca3fb89eebe41ddee515024
Kirill <xfiendx4life@gmail.com>
2022-01-28 18:17:55 +0300

    Jwt middleware added

--- cmd/shrtener/app/api.go
+++ cmd/shrtener/app/api.go
@@ -7,7 +7,8 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
-	"github.com/xfiendx4life/gb_go_backend1/internal/api/middleware"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/xfiendx4life/gb_go_backend1/internal/api/middlewares"
 	"github.com/xfiendx4life/gb_go_backend1/internal/config"
 	urlDel "github.com/xfiendx4life/gb_go_backend1/internal/pkg/url/deliver"
 	urlCase "github.com/xfiendx4life/gb_go_backend1/internal/pkg/url/usecase"
@@ -38,21 +39,27 @@ func App(z *zap.SugaredLogger) {
 	}
 	ctx := context.Background() // TODO Change for context with Timeout
 	server := echo.New()
-	server.Use(middleware.RecoverMiddleware(z))
+	// server.Use(middlewares.RecoverMiddleware(z))
+	server.Use(middleware.Recover())
 	store := storage.New()
 	err = store.InitNewStorage(ctx, z, conf.GetConfStorage())
 	if err != nil {
 		log.Fatalf("can't connect to storage")
 	}
 	user := userCase.New(store)
+	ttl := conf.GetConfAuth().GetTtl()
+	dur := time.Duration(ttl) * time.Minute
+	z.Infof("expiry time %v", dur)
+	z.Infof("expiry time %d", time.Now().Add(dur).Unix())
 	userDeliver := userDel.New(user,
-		time.Now().Add(time.Duration(conf.GetConfAuth().GetTtl())).Unix(),
+		time.Now().Add(dur).Unix(),
 		conf.GetConfAuth().GetSecretKey(),
 		z)
 	url := urlCase.New(store)
 	urlDeliver := urlDel.New(url, z)
 	server.POST("/user/create", userDeliver.Create)
 	server.GET("/user/login", userDeliver.Login)
-	server.POST("/url", urlDeliver.Save)
+	server.POST("/url", urlDeliver.Save, middlewares.JWTAuthMiddleware(conf.GetConfAuth().GetSecretKey()))
 	log.Fatal(server.Start(port))
+	// middleware.REcoverWithConfig()
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,11 @@ require (
 require github.com/golang-jwt/jwt/v4 v4.2.0
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.1 // indirect
